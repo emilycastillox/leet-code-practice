7a11ec3b6d723a425d98ef86a4a215298fa25a8e
Emily Castillo <emilycastillox@gmail.com>
2022-07-19 23:05:47 -0400

    more solutions

--- twoSum.js
+++ twoSum.js
@@ -40,3 +40,30 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
     }
     return resultIndices
 };
+
+
+// other solution - nested for loop, On^2
+
+function twoSum(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] === target) {
+          return [i, j];
+        }
+      }
+    }
+  }
+
+// using map
+
+var twoSum = function(nums, target) {
+    let map = new Map(); //creating hash table
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(pairNum)) {
+            return [map.get(pairNum), i]
+        }     
+        map.set(nums[i], i); // insert an element into the table
+    }
+}
+
